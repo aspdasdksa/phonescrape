27fbf03128833f1a1c46b6fff3e9f75e7162a10b
aspdasdksa <127643094+aspdasdksa@users.noreply.github.com>
2023-08-17 20:44:18 -0400

    Create browser.js

---
+++ browser.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         newgen fuckery
+// @author       You
+// @match        https://receive-sms-free.cc/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const a = document.querySelectorAll('span');
+    const b = /\+358\s\d{10}/g;
+    const c = [];
+    a.forEach(d => {
+        const e = d.textContent;
+        const f = e.match(b);
+        if (f) {
+            c.push(...f);
+        }
+    });
+    console.log('numbers:', c);
+})();
